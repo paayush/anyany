bfeac352e94eb2ccf51fc28df86bef791ec3746b
Aayush pokharel <pokharelaayush199@gmail.com>
2022-08-12 11:57:58 +0545

    personal information

--- anyany.py
+++ anyany.py
@@ -1,5 +1,4 @@
 #my name is aayush pokharel.
 '''
 i am from biratnagar-8. I passed my 10th from b.k.v.m 
-and +2 from arniko college which is the best science college of biratnagar
-'''
+and +2 from arniko college which is the best science college of biratnagar'''
