24d285be840818c34098a422817ca9bddc8bc33a
Aayush pokharel <pokharelaayush199@gmail.com>
2022-08-16 12:01:17 +0545

    practice

--- anyany.py
+++ anyany.py
@@ -2,4 +2,4 @@
 '''
 i am from biratnagar-8. I passed my 10th from b.k.v.m 
 and +2 from arniko college which is the best science college of biratnagar
-'''
+''' 
---
+++ first.py
@@ -0,0 +1,30 @@
+'''
+syntax
+
+    defining function:
+    def function_name():
+    ...you can do anything here in this line...
+    return........... something
+'''
+#calling function:-
+    #function_name()
+
+
+
+
+
+
+
+
+#example
+def duita_sum_joda():
+    sum = 3+5
+    print(sum)
+    #return sum
+
+duita_sum_joda()
+
+
+#taking input form user
+my_name=input("enter your facebook id: ")
+print(my_name)
