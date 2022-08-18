d29dce8849ffd01d6fc7ddf217f4e59a53d750e0
Aayush pokharel <pokharelaayush199@gmail.com>
2022-08-18 10:10:12 +0545

    practice

--- first.py
+++ first.py
@@ -17,14 +17,29 @@ syntax
 
 
 #example
-def duita_sum_joda():
+'''def duita_sum_joda():
     sum = 3+5
     print(sum)
     #return sum
 
 duita_sum_joda()
-
+'''
 
 #taking input form user
-my_name=input("enter your facebook id: ")
-print(my_name)
+'''first_name = input("enter your first name:-")
+second_name = input("enter your second name:-")
+full_name = first_name + second_name
+
+print("your full name is ",full_name)'''
+
+#string formating 
+'''name = input("enter your name:-")
+print(f"your name is {name}")'''
+
+f = -1
+s = -2
+t = -3
+f0 = -4
+fi = -5
+
+'''print(f"my{f} name{s} is{t} aayush{f0} pokharel{fi}")'''
